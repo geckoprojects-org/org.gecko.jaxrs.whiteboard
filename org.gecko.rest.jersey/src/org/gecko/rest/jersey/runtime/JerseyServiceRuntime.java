9e15a5e1c122501643d6a3d89a9282571d7fa104
Mark Hoffmann <m.hoffmann@data-in-motion.biz>
2020-11-13 21:26:43 +0100

    - added jersey.version
    Signed-off-by: Mark Hoffmann <m.hoffmann@data-in-motion.biz>

--- org.gecko.rest.jersey/src/org/gecko/rest/jersey/runtime/JerseyServiceRuntime.java
+++ org.gecko.rest.jersey/src/org/gecko/rest/jersey/runtime/JerseyServiceRuntime.java
@@ -52,7 +52,7 @@ import org.osgi.service.jaxrs.whiteboard.JaxrsWhiteboardConstants;
  */
 @Capability(namespace = ImplementationNamespace.IMPLEMENTATION_NAMESPACE, version = JaxrsWhiteboardConstants.JAX_RS_WHITEBOARD_SPECIFICATION_VERSION, name = JaxrsWhiteboardConstants.JAX_RS_WHITEBOARD_IMPLEMENTATION, attribute = {
 		"uses:=\"javax.ws.rs,javax.ws.rs.sse,javax.ws.rs.core,javax.ws.rs.ext,javax.ws.rs.client,javax.ws.rs.container,org.osgi.service.jaxrs.whiteboard\"",
-		"provider=jersey" })
+		"provider=jersey", "jersey.version=2.32" })
 public class JerseyServiceRuntime extends AbstractJerseyServiceRuntime {
 
 	public enum State {
