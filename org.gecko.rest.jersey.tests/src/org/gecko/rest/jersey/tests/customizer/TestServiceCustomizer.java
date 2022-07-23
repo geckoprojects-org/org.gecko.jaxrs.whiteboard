5082ab16405f82e5f29e1bd2366736a2c865fda6
Mark Hoffmann <m.hoffmann@data-in-motion.biz>
2022-07-23 18:32:43 +0200

    license check
    - fix more missing licenses
    
    Signed-off-by: Mark Hoffmann <m.hoffmann@data-in-motion.biz>

--- org.gecko.rest.jersey.tests/src/org/gecko/rest/jersey/tests/customizer/TestServiceCustomizer.java
+++ org.gecko.rest.jersey.tests/src/org/gecko/rest/jersey/tests/customizer/TestServiceCustomizer.java
@@ -1,13 +1,15 @@
 /**
- * Copyright (c) 2012 - 2018 Data In Motion and others.
+ * Copyright (c) 2012 - 2022 Data In Motion and others.
  * All rights reserved. 
  * 
  * This program and the accompanying materials are made available under the terms of the 
- * Eclipse Public License v1.0 which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
  * 
  * Contributors:
  *     Data In Motion - initial API and implementation
+ *     Stefan Bishof - API and implementation
+ *     Tim Ward - implementation
  */
 package org.gecko.rest.jersey.tests.customizer;
 
