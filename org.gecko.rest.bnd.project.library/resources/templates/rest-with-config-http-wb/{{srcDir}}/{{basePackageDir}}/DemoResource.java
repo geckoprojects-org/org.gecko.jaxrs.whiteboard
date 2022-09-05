964b4e372fee0c9be05f99c906e2a25f7f349241
Mark Hoffmann <m.hoffmann@data-in-motion.biz>
2022-09-05 14:28:01 +0200

    License check
    - added missing licese headers
    - added exclusions for library projects
    
    Signed-off-by: Mark Hoffmann <m.hoffmann@data-in-motion.biz>

--- org.gecko.rest.bnd.project.library/resources/templates/rest-with-config-http-wb/{{srcDir}}/{{basePackageDir}}/DemoResource.java
+++ org.gecko.rest.bnd.project.library/resources/templates/rest-with-config-http-wb/{{srcDir}}/{{basePackageDir}}/DemoResource.java
@@ -1,13 +1,15 @@
 /**
-import org.osgi.service.jaxrs.whiteboard.annotations.RequireJaxrsWhiteboard; * Copyright (c) 2012 - 2018 Data In Motion and others.
+ * Copyright (c) 2012 - 2022 Data In Motion and others.
  * All rights reserved. 
  * 
  * This program and the accompanying materials are made available under the terms of the 
- * Eclipse Public License v1.0 which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
  * 
  * Contributors:
  *     Data In Motion - initial API and implementation
+ *     Stefan Bishof - API and implementation
+ *     Tim Ward - implementation
  */
 package {{basePackageName}};
 
@@ -17,6 +19,7 @@ import javax.ws.rs.Path;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.ServiceScope;
 import org.osgi.service.http.whiteboard.annotations.RequireHttpWhiteboard;
+import org.osgi.service.jaxrs.whiteboard.annotations.RequireJaxrsWhiteboard;
 import org.osgi.service.jaxrs.whiteboard.propertytypes.JaxrsName;
 import org.osgi.service.jaxrs.whiteboard.propertytypes.JaxrsResource;
 
@@ -26,6 +29,7 @@ import org.osgi.service.jaxrs.whiteboard.propertytypes.JaxrsResource;
  * @since 1.0
  */
 @RequireHttpWhiteboard
+@RequireJaxrsWhiteboard;
 @JaxrsResource
 @JaxrsName("demo")
 @Component(service = DemoResource.class, enabled = true, scope = ServiceScope.PROTOTYPE)
