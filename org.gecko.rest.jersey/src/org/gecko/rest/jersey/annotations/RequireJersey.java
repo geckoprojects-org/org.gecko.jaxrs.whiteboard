b211008201dc93671ab3d7be7593ce1506a3acd7
Mark Hoffmann <m.hoffmann@data-in-motion.biz>
2022-11-07 15:32:39 +0100

    License
    Signed-off-by: Mark Hoffmann <m.hoffmann@data-in-motion.biz>

--- org.gecko.rest.jersey/src/org/gecko/rest/jersey/annotations/RequireJersey.java
+++ org.gecko.rest.jersey/src/org/gecko/rest/jersey/annotations/RequireJersey.java
@@ -3,11 +3,13 @@
  * All rights reserved. 
  * 
  * This program and the accompanying materials are made available under the terms of the 
- * Eclipse Public License v1.0 which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
  * 
  * Contributors:
  *     Data In Motion - initial API and implementation
+ *     Stefan Bishof - API and implementation
+ *     Tim Ward - implementation
  */
 package org.gecko.rest.jersey.annotations;
 
